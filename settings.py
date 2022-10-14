25cfa6cb1e3df409a5f230b1ab00c050c5b33295
ktodoy <115736452+ktodoy@users.noreply.github.com>
2022-10-14 13:58:47 -0400

    Creating and importing settings

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    """A class to store all setting for Alien Invasion."""
+    # Screen settings
+    self.screen_width = 1200
+    self.screen_height = 80
+    self.bg_color = (230,230,230)
