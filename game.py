74898af1074fb1a87061ca32089dad8787b5f971
ktodoy <115736452+ktodoy@users.noreply.github.com>
2022-10-19 14:20:21 -0400

    moving right

--- game.py
+++ game.py
@@ -21,6 +21,8 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -31,7 +33,22 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
