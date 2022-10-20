e336af0c2731ce78135f69cce4d84ccda2cd6f3a
ktodoy <115736452+ktodoy@users.noreply.github.com>
2022-10-20 15:04:52 -0400

    move left and right

--- game.py
+++ game.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -41,7 +42,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
